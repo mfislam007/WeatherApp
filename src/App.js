5972bd2451344daa3c1b629cdca380f3c58ed250
Farukul Islam <mfislam2010280007@gmail.com>
2023-03-01 18:11:49 +0200

    weather app first commit

--- src/App.js
+++ src/App.js
@@ -5,19 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Weather App</h1>
       </header>
+      <main></main>
+      <footer>Page created by Md Farukul Islam</footer>
     </div>
   );
 }
